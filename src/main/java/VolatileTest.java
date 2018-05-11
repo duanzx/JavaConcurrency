7b6eb869b5092aeee324c7ad984849d628998f7d
duanzx <15726645490@163.com>
2018-05-11 14:11:25 +0800

    JAVA

---
+++ src/main/java/VolatileTest.java
@@ -0,0 +1,37 @@
+/**
+ * Created by duanzx on 2018/5/11.
+ */
+public class VolatileTest extends Thread {
+//    private volatile boolean running=true;
+    private boolean running=true;
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    @Override
+    public void run(){
+        System.out.println(System.currentTimeMillis());
+        System.out.println("run--------------------");
+        while (running){
+//            System.out.println("true-----------------------");
+        }
+        System.out.println(System.currentTimeMillis());
+        System.out.println("false--------------------------");
+    }
+
+    public static void main(String[] args)throws Exception{
+        VolatileTest volatileTest = new VolatileTest();
+        volatileTest.start();
+        Thread.sleep(3000);
+        volatileTest.setRunning(false); // 此时 volatile线程并没有结束，并且线程里的runing=true，没有改变
+        System.out.println("set runing = false");
+//        Thread.sleep(1000);
+//        System.out.println(volatileTest.running);
+
+    }
+}
