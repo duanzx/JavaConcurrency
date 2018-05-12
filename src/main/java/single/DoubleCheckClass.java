fa9c1cd5e6878b198dd602d149a54602d556e848
duanzx <15726645490@163.com>
2018-05-12 09:45:32 +0800

    单例模式：静态内部类，双重确认

--- src/main/java/single/DoubleCheckClass.java
+++ src/main/java/single/DoubleCheckClass.java
@@ -1,4 +1,45 @@
 package single;
 
 public class DoubleCheckClass {
+    private static DoubleCheckClass singleClass;
+
+    public static DoubleCheckClass getInstance() {
+        if (singleClass == null) {
+            try {
+                Thread.sleep(3000);//初始化对象所花费的时间
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (DoubleCheckClass.class) {
+                if (singleClass == null) {
+                    singleClass = new DoubleCheckClass();
+                }
+            }
+        }
+        return singleClass;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(DoubleCheckClass.getInstance().hashCode());
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(DoubleCheckClass.getInstance().hashCode());
+            }
+        });
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(DoubleCheckClass.getInstance().hashCode());
+            }
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
