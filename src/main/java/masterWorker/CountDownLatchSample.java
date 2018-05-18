9fd795d1a8b9bc1cee588a95be513217b65b0cd9
duanzx <15726645490@163.com>
2018-05-18 10:09:05 +0800

    Master Work

--- src/main/java/masterWorker/CountDownLatchSample.java
+++ src/main/java/masterWorker/CountDownLatchSample.java
@@ -11,19 +11,19 @@ public class CountDownLatchSample {
 
     public static void main(String[] args)throws Exception{
         final CountDownLatch countDownLatch = new CountDownLatch(10);
-        for(int x=0;x<10;x++){
             new Thread(new Runnable() {
                 public void run() {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                    for(int x=0;x<10;x++){
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("正在异步执行，耗时5秒钟");
+                        countDownLatch.countDown();
                     }
-                    System.out.println("正在异步执行，耗时一秒钟");
-                    countDownLatch.countDown();
                 }
             }).start();
-        }
         countDownLatch.await();
         System.out.println("异步执行结束-----------");
     }
