0692e0d6f62ad57e9188ee46d784b12319f75049
duanzx <15726645490@163.com>
2018-05-18 14:49:55 +0800

    线程池

--- src/main/java/threadpool/producerAndConsumer/Consumer.java
+++ src/main/java/threadpool/producerAndConsumer/Consumer.java
@@ -1,7 +1,28 @@
 package threadpool.producerAndConsumer;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * Created by duanzx on 2018/5/18.
  */
-public class Consumer {
+public class Consumer implements Runnable{
+    private BlockingQueue<String> blockingQueue;
+    private static Random random = new Random();
+
+    public Consumer(BlockingQueue blockingQueue){
+        this.blockingQueue=blockingQueue;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            String element = blockingQueue.take();
+            Thread.sleep(random.nextInt(1000));
+            System.out.println(Thread.currentThread().getName()+"获取到元素"+element + "，消费成功");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/threadpool/producerAndConsumer/MainClass.java
+++ src/main/java/threadpool/producerAndConsumer/MainClass.java
@@ -1,7 +1,31 @@
 package threadpool.producerAndConsumer;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+
 /**
  * Created by duanzx on 2018/5/18.
  */
 public class MainClass {
+
+    public static void main(String[] args)throws Exception{
+        BlockingQueue<String> blockingQueue= new LinkedBlockingDeque<>(10);
+        Producer p1 = new Producer(blockingQueue);
+        Producer p2 = new Producer(blockingQueue);
+        Producer p3 = new Producer(blockingQueue);
+        Consumer c1 = new Consumer(blockingQueue);
+        Consumer c2 = new Consumer(blockingQueue);
+        Consumer c3 = new Consumer(blockingQueue);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(p1);
+        executorService.execute(p2);
+        executorService.execute(p3);
+        executorService.execute(c1);
+        executorService.execute(c2);
+        executorService.execute(c3);
+        Thread.sleep(5000);
+
+    }
 }
