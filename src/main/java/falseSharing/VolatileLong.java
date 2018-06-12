713dbe781fd3d8eca2629ab9905b4b0a9f610aa0
duanzx <15726645490@163.com>
2018-06-12 16:57:21 +0800

    线程池

---
+++ src/main/java/falseSharing/VolatileLong.java
@@ -0,0 +1,9 @@
+package falseSharing;
+
+/**
+ * Created by duanzx on 2018/6/12.
+ */
+public final class VolatileLong {
+    public volatile long value = 0L;
+    public long p1, p2, p3, p4, p5, p6;
+}
