1da725311b0f999960306a6ec19542067d881ecf
duanzx <15726645490@163.com>
2018-12-19 07:33:04 +0800

    数据结构与算法 - 栈实现

--- src/main/java/datastruct/stack/JLinkedStack.java
+++ src/main/java/datastruct/stack/JLinkedStack.java
@@ -6,4 +6,20 @@ package datastruct.stack;
 public class JLinkedStack {
     private Object[] items;
     private int head;
+
+    public JLinkedStack() {
+        this.items = new Object[Integer.MAX_VALUE];
+        this.head = -1;
+    }
+
+    public boolean push(Object item) {
+
+
+        return false;
+    }
+
+    public Object pop() {
+        return null;
+    }
+
 }
