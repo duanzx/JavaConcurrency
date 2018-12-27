157de1fcbbf762d91c02cf1855257dc214246c24
duanzx <15726645490@163.com>
2018-12-27 23:53:28 +0800

    数据结构与算法 - 二叉树定义

--- src/main/java/datastruct/bintree/java/BinNode.java
+++ src/main/java/datastruct/bintree/java/BinNode.java
@@ -1,4 +1,19 @@
 package datastruct.bintree.java;
 
-public class BinNode {
+/**
+ * 定义二叉树的结构
+ */
+public interface BinNode {
+
+
+    //while 一直向上更新节点高度
+    public void updateAbove();
+
+    public void insertAsRc();
+
+    public void insertAsLc();
+
+    //先序遍历，按照某种次序访问树中各个节点，每个节点被访问恰好一次
+    public void
+
 }
